63535ddb00fc89de899d5871e74309bce7f0aa50
acie-arsh <117077675+acie-arsh@users.noreply.github.com>
2023-01-12 23:39:09 +0530

    Update script.js

--- 20_days_of_code/day3/script.js
+++ 20_days_of_code/day3/script.js
@@ -1,13 +1,15 @@
 function palindrome() {
     var inputstr = document.getElementById("input").value;
-    var reg = /[W_]/G;
-    var lcase = inputstr.toLowerCase().replace(Reg, "");
+    var reg = /[w_]/g;
+    var lcase = inputstr.toLowerCase().replace(reg, "");
     var reversed = lcase.split("").reverse().join("");
 
-    if(reversed == lcase){
-        document.getElementById("result").innerHTML = "Given String is a Palindrome";
+    if(reversed === lcase){
+        document.getElementById("result").innerHTML = 
+            "Given String is a Palindrome";
     }
     else{
-        document.getElementById("result").innerHTML = "Given String is not a Palindrome";
+        document.getElementById("result").innerHTML = 
+            "Given String is not a Palindrome";
     }
 }
