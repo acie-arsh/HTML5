eee9d401d0970eae1227170dd44b365925bb91db
acie-arsh <117077675+acie-arsh@users.noreply.github.com>
2023-01-19 22:58:50 +0530

    Add files via upload

---
+++ 20_days_of_code/day10/script.js
@@ -0,0 +1,13 @@
+function changesec1(){
+    document.getElementById("content1").classList.toggle("fontexpand");
+
+
+}
+
+function changesec2(){
+    document.getElementById("content2").classList.toggle("fontexpand");
+}
+
+function changesec3(){
+    document.getElementById("content3").classList.toggle("fontexpand");
+}
