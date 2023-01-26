68f29eebee482b26cca9c1fc7f5d3860ef388ba4
acie-arsh <117077675+acie-arsh@users.noreply.github.com>
2023-01-26 23:50:47 +0530

    Add files via upload

--- 20_days_of_code/days 11 to 20/js/Player.js
+++ 20_days_of_code/days 11 to 20/js/Player.js
@@ -1,26 +1,56 @@
-class Player{
-    constructor({ position, collisionBlocks }){
+class Player extends Sprite{
+    constructor({ position, collisionBlocks, imageSrc, frameRate, scale = 0.5 }){
+        super({ imageSrc, frameRate, scale })
         this.position = position;
         this.velocity = {
             x:0, y:1,
         }
-        this.width = 100/4;
-        this.height = 100/4;
+     
         this.collisionBlocks = collisionBlocks;
+        this.hitbox = {
+            position : {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            width : 10,
+            height : 10,
+        }
     }
 
-    draw(){
-        c.fillStyle = "lime";
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
+
 
     update(){
+        this.updateFrames();
+        this.updateHitbox();
+        
+        //this is to draw a box around image and the hitbox respectively
+
+        // c.fillStyle = 'rgba(0, 255, 0, 0.2)'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // c.fillStyle = 'rgba(100, 0 ,0, 0.5)'
+        // c.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height)
+
+
         this.draw();
         this.position.x += this.velocity.x;
+        this.updateHitbox();
         this.checkForHorizontalCollisions();
         this.applyGravity();
+        this.updateHitbox();
         this.checkForVerticalCollisions();
         
+    }
+
+    updateHitbox(){
+        this.hitbox = {
+            position : {
+                x: this.position.x + 35,
+                y: this.position.y + 26,
+            },
+            width : 15,
+            height : 27,
+        }
     }
 
     checkForHorizontalCollisions(){
@@ -29,20 +59,26 @@ class Player{
 
             if(
                 collision({
-                    object1 : this,
+                    object1 : this.hitbox,
                     object2 : collisionBlock,
                 })
                 
                 ){ 
                     if(this.velocity.x > 0){
                         this.velocity.x = 0;
-                        this.position.x = collisionBlock.position.x - this.width - 0.01;
+
+                        const offset = this.hitbox.position.x - this.position.x + this.hitbox.width;
+
+                        this.position.x = collisionBlock.position.x - this.width - offset -  0.01;
                         break;
                     }
 
                     if(this.velocity.x < 0){
                         this.velocity.x = 0;
-                        this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01;
+
+                        const offset = this.hitbox.position.x - this.position.x
+
+                        this.position.x = collisionBlock.position.x + collisionBlock.width - offset + 0.01;
                         break;
                     }
 
@@ -62,20 +98,26 @@ class Player{
 
             if(
                 collision({
-                    object1 : this,
+                    object1 : this.hitbox,
                     object2 : collisionBlock,
                 })
                 
                 ){ 
                     if(this.velocity.y > 0){
                         this.velocity.y = 0;
-                        this.position.y = collisionBlock.position.y - this.height - 0.01;
+                        
+                        const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+                        
+                        this.position.y = collisionBlock.position.y - offset - 0.01;
                         break;
                     }
 
                     if(this.velocity.y < 0){
                         this.velocity.y = 0;
-                        this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01;
+
+                        const offset = this.hitbox.position.y - this.position.y + this.hitbox.height;
+
+                        this.position.y = collisionBlock.position.y + collisionBlock.height - offset + 0.01;
                         break;
                     }
 
