05ab576ef52cf41d05a13bdde543cec2fa352e5d
acie-arsh <117077675+acie-arsh@users.noreply.github.com>
2023-01-27 23:25:12 +0530

    Add files via upload

--- 20_days_of_code/days 11 to 20/script.js
+++ 20_days_of_code/days 11 to 20/script.js
@@ -73,6 +73,53 @@ const player = new Player({
     imageSrc: './img/warrior/Idle.png',
     frameRate: 8,
     
+    animations : {
+        Idle: {
+            imageSrc: './img/warrior/Idle.png',
+            frameRate: 8,
+            frameBuffer: 15,
+        },
+        IdleLeft: {
+            imageSrc: './img/warrior/IdleLeft.png',
+            frameRate: 8,
+            frameBuffer: 15,
+        },
+        Run: {
+            imageSrc: './img/warrior/Run.png',
+            frameRate: 8,
+            frameBuffer: 10,
+        },
+        RunLeft: {
+            imageSrc: './img/warrior/RunLeft.png',
+            frameRate: 8,
+            frameBuffer: 10,
+        },
+        Jump: {
+            imageSrc: './img/warrior/Jump.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        JumpLeft: {
+            imageSrc: './img/warrior/JumpLeft.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        Fall: {
+            imageSrc: './img/warrior/Fall.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        FallLeft: {
+            imageSrc: './img/warrior/FallLeft.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        
+        
+       
+        
+
+    }
     
 });
 
@@ -108,8 +155,30 @@ function animate(){
     
 
     player.velocity.x = 0;
-    if(keys.d.pressed) player.velocity.x = 4;
-    else if(keys.a.pressed) player.velocity.x = -4;
+    if(keys.d.pressed) {
+        player.lastDirection = 'right'
+        player.switchSprite('Run')
+        player.velocity.x = 1.5;
+    }
+    else if(keys.a.pressed) {
+        player.lastDirection = 'left'
+        player.switchSprite('RunLeft')
+        player.velocity.x = -1.5;
+    }
+    
+    else if(player.velocity.y === 0) {
+        if(player.lastDirection === 'right') player.switchSprite('Idle');
+        else player.switchSprite('IdleLeft')
+    }
+
+    if(player.velocity.y < 0) {
+        if(player.lastDirection === 'right')player.switchSprite('Jump');
+        else player.switchSprite('JumpLeft')
+    }
+        else if(player.velocity.y > 0) {
+            if(player.lastDirection === 'right') player.switchSprite('Fall')
+            else player.switchSprite('FallLeft')
+    };
     
     c.restore()
 
@@ -143,7 +212,7 @@ window.addEventListener('keydown', (event) =>{
             break
 
         case 'w':
-        player.velocity.y = -8;
+        player.velocity.y = -8
             break
     }
 
